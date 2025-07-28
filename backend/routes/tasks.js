38751272c354f2257a4b6fd251b12a60d8bd1cf6
Yasmine <yasminemohd56@gmail.com>
2025-07-28 16:44:44 +0300

    Task update

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -56,4 +56,34 @@ router.get('/tasks/:id', async (req, res) => {
     return res.status(500).json({ success: false, message: 'Server error.' });
   }
 });
+
+router.put('/tasks/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title, description, status, estimate_time, logged_time } = req.body;
+  try {
+    const task = await Task.findByPk(id);
+    if (!task) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'Task not found.' });
+    }
+
+    task.title = title || task.title;
+    task.description = description || task.description;
+    task.status = status || task.status;
+    task.estimate_time = estimate_time || task.estimate_time;
+    task.logged_time = logged_time || task.logged_time;
+
+    await task.save();
+    return res.status(200).json({
+      success: true,
+      message: 'Task updated successfully.',
+      task,
+    });
+  } catch (err) {
+    console.error('Error updating task:', err);
+    return res.status(500).json({ success: false, message: 'Server error.' });
+  }
+});
+
 module.exports = router;
